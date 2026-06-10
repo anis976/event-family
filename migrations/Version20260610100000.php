b0e6e83a84b1705b61af505d77bbe9ff95a1c251
anis976 <anissoir@hotmail.fr>
2026-06-11 01:29:27 +0200

    fix: migration verification_token idempotente

--- migrations/Version20260610100000.php
+++ migrations/Version20260610100000.php
@@ -16,9 +16,19 @@ final class Version20260610100000 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql('ALTER TABLE ef_users ADD verification_token_hash VARCHAR(64) DEFAULT NULL');
-        $this->addSql('ALTER TABLE ef_users ADD verification_token_expires_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
-        $this->addSql('CREATE UNIQUE INDEX uniq_ef_users_verification_token ON ef_users (verification_token_hash)');
+        $table = $this->connection->createSchemaManager()->introspectTable('ef_users');
+
+        if (!$table->hasColumn('verification_token_hash')) {
+            $this->addSql('ALTER TABLE ef_users ADD verification_token_hash VARCHAR(64) DEFAULT NULL');
+        }
+
+        if (!$table->hasColumn('verification_token_expires_at')) {
+            $this->addSql('ALTER TABLE ef_users ADD verification_token_expires_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
+        }
+
+        if (!$table->hasIndex('uniq_ef_users_verification_token')) {
+            $this->addSql('CREATE UNIQUE INDEX uniq_ef_users_verification_token ON ef_users (verification_token_hash)');
+        }
     }
 
     public function down(Schema $schema): void
